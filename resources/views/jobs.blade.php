95e28baac8bea9b4550c2cac398fa233eaab9a10
shiamsharif <shiam.sharif.07@gmail.com>
2024-05-02 13:55:18 +0600

    Day :13 (2:29)

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,8 +5,11 @@
     
     <div class="">
         @foreach ($jobs as $job)
-                <a href="/jobs/{{ $job['id'] }}" class="text-purple-500  block px-4 border border-gray-300 p-4 mb-2 rounded-lg ">
-                    <strong>{{ $job['title'] }} : </strong> Pays {{ $job['salary'] }} per year.
+                <a href="/jobs/{{ $job['id'] }}" class="text-purple-500 bg-yellow-100 hover:bg-yellow-200 block px-4 border border-gray-300 p-4 mb-2 rounded-lg ">
+                    <div class="font-bold text-cyan-400">{{ $job->employer->name }}</div>
+                    <div>
+                        <strong>{{ $job['title'] }} : </strong> Pays {{ $job['salary'] }} per year.
+                    </div>
                 </a>
         @endforeach
     </div>
