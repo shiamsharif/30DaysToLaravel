407a5f00114da1603f229667b648c20b76ce6492
shiamsharif <shiam.sharif.07@gmail.com>
2024-05-04 16:14:40 +0600

    Day : 19

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,16 @@ use App\Models\Job;
 Route::view('/', 'home');
 
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs',  'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs',  'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
 
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
