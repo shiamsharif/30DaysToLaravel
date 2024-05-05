fcc984b297769eed63c6aaffe6732e66c2c698ef
shiamsharif <shiam.sharif.07@gmail.com>
2024-05-05 11:28:15 +0600

    Add: blade components in create file

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -15,17 +15,12 @@
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
           <div class="sm:col-span-4">
-            <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+            <x-form-label for="title" >Title</x-form-label>
             <div class="mt-2">
-              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                
-                <input type="text" name="title" id="title" class="block flex-1 border-0 px-3 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder=" Title " required>
-              </div>
+              <x-form-input name="title" id="title" placeholder="CEO" required ></x-form-input>
 
               {{-- Validation: --}}
-              @error('title')
-                <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-              @enderror
+              <x-form-error name="title" ></x-form-error>
             </div>
           </div>
 
